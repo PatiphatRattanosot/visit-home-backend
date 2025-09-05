4eb4640144f52de920eeaede0ab6be49f0d3e593
patiphatrattanosot <654259017@webmail.npru.ac.th>
2025-09-06 04:39:43 +0700

    feat: enhance SDQ model and controller

--- src/controllers/sdq_controller.ts
+++ src/controllers/sdq_controller.ts
@@ -1,12 +1,12 @@
 import { Elysia, t } from "elysia";
-import SDQModel from "../models/sdq_model";
+import SDQModel, { ISDQ } from "../models/sdq_model";
 // create
 const create_sdq = (app: Elysia) =>
     app.post(
         "/",
         async ({ body, set }) => {
             try {
-                const { student_id, year_id } = body;
+                const { student_id, year_id, emotional, behavioral, hyperactivity, friendship, social } = body;
                 // ตรวจสอบว่ามี student_id หรือไม่
                 if (!student_id) {
                     set.status = 400;
@@ -25,70 +25,109 @@ const create_sdq = (app: Elysia) =>
                 }
                 // สร้างข้อมูล SDQ ใหม่
                 const sdq = new SDQModel(body);
+
+                // คำนวณคะแนนรวมด้านอารมณ์
+                const { question_3, question_8, question_13, question_16, question_24 } = emotional;
+                const emotional_total = (question_3 + question_8 + question_13 + question_16 + question_24)
+
+                // คำนวณคะแนนรวมด้านพฤติกรรม
+                const { question_5, question_7, question_12, question_18, question_22 } = behavioral;
+                const behavioral_total = (question_5 + question_7 + question_12 + question_18 + question_22)
+
+                // คำนวณคะแนนรวมด้านสมาธิสั้น
+                const { question_2, question_10, question_15, question_21, question_25 } = hyperactivity;
+                const hyperactivity_total = (question_2 + question_10 + question_15 + question_21 + question_25)
+
+                // คำนวณคะแนนรวมด้านความสัมพันธ์กับเพื่อน
+                const { question_6, question_11, question_14, question_19, question_23 } = friendship;
+                const friendship_total = (question_6 + question_11 + question_14 + question_19 + question_23)
+
+                // คำนวณคะแนนรวมด้านสังคม
+                const { question_1, question_4, question_9, question_17, question_20 } = social;
+                const social_total = (question_1 + question_4 + question_9 + question_17 + question_20)
+
+                sdq.emotional.total_score = Number(emotional_total);
+                sdq.behavioral.total_score = Number(behavioral_total);
+                sdq.hyperactivity.total_score = Number(hyperactivity_total);
+                sdq.friendship.total_score = Number(friendship_total);
+                sdq.social.total_score = Number(social_total);
+
                 sdq.status = true;
                 await sdq.save();
                 set.status = 201;
                 return { message: "เพิ่มข้อมูล SDQ สำเร็จ", sdq };
             } catch (error) {
                 set.status = 500;
-                return {message:"เซิฟเวอร์เกิดข้อผิดพลาดไม่สามารถเพิ่มข้อมูล SDQ ได้"}
+                return { message: "เซิฟเวอร์เกิดข้อผิดพลาดไม่สามารถเพิ่มข้อมูล SDQ ได้" }
             }
-        },{
-            body: t.Object({
-                student_id: t.String(),
-                year_id: t.String(),
-                comment: t.Optional(t.String()),
-                assessor: t.Optional(t.String()),
-                question: t.Optional(
-                    t.Object({
-                        question_1: t.Number(),
-                        question_2: t.Number(),
-                        question_3: t.Number(),
-                        question_4: t.Number(),
-                        question_5: t.Number(),
-                        question_6: t.Number(),
-                        question_7: t.Number(),
-                        question_8: t.Number(),
-                        question_9: t.Number(),
-                        question_10: t.Number(),
-                        question_11: t.Number(),
-                        question_12: t.Number(),
-                        question_13: t.Number(),
-                        question_14: t.Number(),
-                        question_15: t.Number(),
-                        question_16: t.Number(),
-                        question_17: t.Number(),
-                        question_18: t.Number(),
-                        question_19: t.Number(),
-                        question_20: t.Number(),
-                        question_21: t.Number(),
-                        question_22: t.Number(),
-                        question_23: t.Number(),
-                        question_24: t.Number(),
-                        question_25: t.Number(),
-                    })
-                ),
-                
+        }, {
+        body: t.Object({
+            student_id: t.String(),
+            year_id: t.String(),
+            assessor: t.String(),
+            emotional: t.Object({
+                question_3: t.String(),
+                question_8: t.String(),
+                question_13: t.String(),
+                question_16: t.String(),
+                question_24: t.String(),
             }),
-            detail: {
-                tags: ["SDQ"],
-                description: "สร้างข้อมูล SDQ",
-            },
-        })
+            behavioral: t.Object({
+                question_5: t.String(),
+                question_7: t.String(),
+                question_12: t.String(),
+                question_18: t.String(),
+                question_22: t.String(),
+            }),
+            hyperactivity: t.Object({
+                question_2: t.String(),
+                question_10: t.String(),
+                question_15: t.String(),
+                question_21: t.String(),
+                question_25: t.String(),
+            }),
+            friendship: t.Object({
+                question_6: t.String(),
+                question_11: t.String(),
+                question_14: t.String(),
+                question_19: t.String(),
+                question_23: t.String(),
+            }),
+            social: t.Object({
+                question_1: t.String(),
+                question_4: t.String(),
+                question_9: t.String(),
+                question_17: t.String(),
+                question_20: t.String(),
+            }),
+            other: t.Object({
+                additional: t.String(),
+                overall_problem: t.String(),
+                problem_time: t.String(),
+                is_uneasy_student: t.String(),
+                is_annoy_student: t.String(),
+                is_difficult_student: t.String(),
+            })
+        }),
+        detail: {
+            tags: ["SDQ"],
+            description: "สร้างข้อมูล SDQ",
+        },
+    })
 
-        //ดึงข้อมูล SDQ ตาม student_id และ year_id
-    const get_sdq_by_student_year_and_assessor = (app: Elysia) =>
+//ดึงข้อมูล SDQ ตาม student_id และ year_id
+const get_sdq_by_student_year_and_assessor = (app: Elysia) =>
     app.post(
         "/by-student-year",
         async ({ body, set }) => {
             try {
-                const { student_id, year_id , assessor } = body;
+                const { student_id, year_id, assessor } = body;
                 if (!student_id || !year_id) {
                     set.status = 400;
                     return { message: "กรุณาระบุ student_id และ year_id" };
                 }
                 // populate student and year
-                const sdq = await SDQModel.findOne({ student_id, year_id,assessor })
+                const sdq = await SDQModel.findOne({ student_id, year_id, assessor })
                     .populate("student_id", "first_name last_name")
                     .populate("year_id", "year");
                 if (!sdq) {
@@ -96,22 +135,22 @@ const create_sdq = (app: Elysia) =>
                     return { message: "ไม่พบข้อมูล SDQ" };
                 }
                 set.status = 200;
-                return {message: "ดึงข้อมูล SDQ สำเร็จ", sdq };
+                return { message: "ดึงข้อมูล SDQ สำเร็จ", sdq };
             } catch (error) {
                 set.status = 500;
                 return { message: "เกิดข้อผิดพลาด", error };
             }
-        },{
-            body: t.Object({
-                student_id: t.String(),
-                year_id: t.String(),
-                assessor: t.String()
-            }),
-            detail: {
-                tags: ["SDQ"],
-                description: "ดึงข้อมูล SDQ ตาม student_id และ year_id",
-            },
-        }
+        }, {
+        body: t.Object({
+            student_id: t.String(),
+            year_id: t.String(),
+            assessor: t.String()
+        }),
+        detail: {
+            tags: ["SDQ"],
+            description: "ดึงข้อมูล SDQ ตาม student_id และ year_id",
+        },
+    }
     );
 
 
@@ -120,7 +159,7 @@ const create_sdq = (app: Elysia) =>
 const delete_sdq = (app: Elysia) =>
     app.delete(
         "/",
-        async ({ body:{sdq_id}, set }) => {
+        async ({ body: { sdq_id }, set }) => {
             try {
                 const result = await SDQModel.findByIdAndDelete(sdq_id);
                 if (!result) {
--- src/models/sdq_model.ts
+++ src/models/sdq_model.ts
@@ -1,44 +1,139 @@
 import { Schema, model, Document } from "mongoose";
 
-const SDQCRITERIA = [0, 1, 2];
+interface ISDQ extends Document {
+  status: boolean;
+  student_id: Schema.Types.ObjectId;
+  year_id: Schema.Types.ObjectId;
+  assessor: string;
+  emotional: {
+    // Group 1: ด้านอารมณ์ Emotional
+    question_3: string;
+    question_8: string;
+    question_13: string;
+    question_16: string;
+    question_24: string;
+    total_score: number;
+  },
+  behavioral: {
+    // Group 2: ด้านพฤติกรรม Behavioral
+    question_5: string;
+    question_7: string;
+    question_12: string;
+    question_18: string;
+    question_22: string;
+    total_score: number;
+  },
+  hyperactivity: {
+    // Group 3: ด้านสมาธิสั้น Hyperactivity
+    question_2: string;
+    question_10: string;
+    question_15: string;
+    question_21: string;
+    question_25: string;
+    total_score: number;
+  },
+  friendship: {
+    // Group 4: ด้านความสัมพันธ์กับเพื่อน Friendship
+    question_6: string;
+    question_11: string;
+    question_14: string;
+    question_19: string;
+    question_23: string;
+    total_score: number;
+  },
+  social: {
+    // Group 5: ด้านความสัมพันธ์กับสังคม Social
+    question_1: string;
+    question_4: string;
+    question_9: string;
+    question_17: string;
+    question_20: string;
+    total_score: number;
+  }
+  other: {
+    additional: "",
+    overall_problem: "", // 0 = ไม่, 1 = ใช่ มีปัญหาเล็กน้อย, 2 = ใช่ มีปัญหาชัดเจน, 3 = ใช่ มีปัญหามาก
+    problem_time: "", // 0 = น้อยกว่า 1 เดือน, 1 = 1-5 เดือน, 2 = 6-12 เดือน, 3 = มากกว่า 1 ปี
+    is_uneasy_student: "", // 0 = ไม่, 1 = เล็กน้อย, 2 = ค่อนข้างมาก, 3 = มาก
+    is_annoy_student: "", // 0 = ไม่, 1 = เล็กน้อย, 2 = ค่อนข้างมาก, 3 = มาก
+    is_difficult_student: "", // 0 = ไม่, 1 = เล็กน้อย, 2 = ค่อนข้างมาก, 3 = มาก
+  }
+}
 
-const SDQSchema = new Schema(
+const SDQCRITERIA = ["0", "1", "2"];
+const OTHER = ["0", "1", "2", "3"];
+
+const SDQSchema = new Schema<ISDQ>(
   {
     status: { type: Boolean, default: false },
     student_id: { type: Schema.Types.ObjectId, ref: "Student", required: true },
     year_id: { type: Schema.Types.ObjectId, ref: "Year", required: true },
-    comment: { type: String, default: "" },
     assessor: { type: String, enum: ["Teacher", "Student", "Parent"] },
-    question: {
-      question_1: { type: Number, enum: SDQCRITERIA },
-      question_2: { type: Number, enum: SDQCRITERIA },
-      question_3: { type: Number, enum: SDQCRITERIA },
-      question_4: { type: Number, enum: SDQCRITERIA },
-      question_5: { type: Number, enum: SDQCRITERIA },
-      question_6: { type: Number, enum: SDQCRITERIA },
-      question_7: { type: Number, enum: SDQCRITERIA },
-      question_8: { type: Number, enum: SDQCRITERIA },
-      question_9: { type: Number, enum: SDQCRITERIA },
-      question_10: { type: Number, enum: SDQCRITERIA },
-      question_11: { type: Number, enum: SDQCRITERIA },
-      question_12: { type: Number, enum: SDQCRITERIA },
-      question_13: { type: Number, enum: SDQCRITERIA },
-      question_14: { type: Number, enum: SDQCRITERIA },
-      question_15: { type: Number, enum: SDQCRITERIA },
-      question_16: { type: Number, enum: SDQCRITERIA },
-      question_17: { type: Number, enum: SDQCRITERIA },
-      question_18: { type: Number, enum: SDQCRITERIA },
-      question_19: { type: Number, enum: SDQCRITERIA },
-      question_20: { type: Number, enum: SDQCRITERIA },
-      question_21: { type: Number, enum: SDQCRITERIA },
-      question_22: { type: Number, enum: SDQCRITERIA },
-      question_23: { type: Number, enum: SDQCRITERIA },
-      question_24: { type: Number, enum: SDQCRITERIA },
-      question_25: { type: Number, enum: SDQCRITERIA },
+    emotional: {
+      question_3: { type: String, enum: SDQCRITERIA, required: true },
+      question_8: { type: String, enum: SDQCRITERIA, required: true },
+      question_13: { type: String, enum: SDQCRITERIA, required: true },
+      question_16: { type: String, enum: SDQCRITERIA, required: true },
+      question_24: { type: String, enum: SDQCRITERIA, required: true },
+      total_score: { type: Number, default: 0 },
+    },
+    behavioral: {
+      question_5: { type: String, enum: SDQCRITERIA, required: true },
+      question_7: { type: String, enum: SDQCRITERIA, required: true },
+      question_12: { type: String, enum: SDQCRITERIA, required: true },
+      question_18: { type: String, enum: SDQCRITERIA, required: true },
+      question_22: { type: String, enum: SDQCRITERIA, required: true },
+      total_score: { type: Number, default: 0 },
+    },
+    hyperactivity: {
+      question_2: { type: String, enum: SDQCRITERIA, required: true },
+      question_10: { type: String, enum: SDQCRITERIA, required: true },
+      question_15: { type: String, enum: SDQCRITERIA, required: true },
+      question_21: { type: String, enum: SDQCRITERIA, required: true },
+      question_25: { type: String, enum: SDQCRITERIA, required: true },
+      total_score: { type: Number, default: 0 },
+    },
+    friendship: {
+      question_6: { type: String, enum: SDQCRITERIA, required: true },
+      question_11: { type: String, enum: SDQCRITERIA, required: true },
+      question_14: { type: String, enum: SDQCRITERIA, required: true },
+      question_19: { type: String, enum: SDQCRITERIA, required: true },
+      question_23: { type: String, enum: SDQCRITERIA, required: true },
+      total_score: { type: Number, default: 0 },
+    },
+    social: {
+      question_1: { type: String, enum: SDQCRITERIA, required: true },
+      question_4: { type: String, enum: SDQCRITERIA, required: true },
+      question_9: { type: String, enum: SDQCRITERIA, required: true },
+      question_17: { type: String, enum: SDQCRITERIA, required: true },
+      question_20: { type: String, enum: SDQCRITERIA, required: true },
+      total_score: { type: Number, default: 0 },
+    },
+
+    other: {
+      additional: { type: String, required: true },
+      overall_problem: { type: String, enum: OTHER, required: true },
+      problem_time: { type: String, enum: OTHER, required: true },
+      is_uneasy_student: {
+        type: String,
+        enum: OTHER,
+        required: true
+      },
+      is_annoy_student: {
+        type: String,
+        enum: OTHER,
+        required: true
+      },
+      is_difficult_student: {
+        type: String,
+        enum: OTHER,
+        required: true
+      },
     },
   },
   { timestamps: true }
 );
 
-const SDQModel = model("SDQ", SDQSchema);
+const SDQModel = model<ISDQ>("SDQ", SDQSchema);
 export default SDQModel;
+export { ISDQ };
